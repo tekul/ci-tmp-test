1fe16155f46e42eda13e1452c21e46a7bc6bf5ff
Luke Taylor <tekul@pm.me>
2023-05-23 17:56:43 +0100

    Create project dir in tmp folder

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -10,5 +10,9 @@ fn exo_smoke_tests() {
     assert!(tmp_dir.path().exists(), "Tmp dir not found");
     assert!(tmp_dir.path().is_dir(), "Tmp dir not a directory");
 
-    let _project_dir = PathBuf::from(tmp_dir.path());
+    let mut project_dir = PathBuf::from(tmp_dir.path());
+    project_dir.push("mariposas");
+    std::fs::create_dir(project_dir.clone()).expect("Failed to create project_dir");
+    assert!(project_dir.exists(), "Project dir {project_dir:?} not found");
+    assert!(project_dir.is_dir(), "Project dir {project_dir:?} not a directory");
 }
