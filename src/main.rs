b9f254397650bf2d030e6cf36a7d0646527436ac
Luke Taylor <tekul@pm.me>
2023-05-23 19:33:46 +0100

    Create project folder in binary
    - Call binary from tests.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{fs::create_dir_all, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    println!("{args:?}");
+    let project_dir = args.get(1).expect("Need to pass directory name");
+    let path: PathBuf = project_dir.into();
+
+    let src_path = path.join("src");
+    println!("Path to src dir: {src_path:?}");
+    create_dir_all(src_path).expect("Failed to create src dir");
 }
--- tests/smoke.rs
+++ tests/smoke.rs
@@ -1,18 +1,52 @@
-use std::path::PathBuf;
+use std::{
+    path::{Path, PathBuf},
+    process::Command,
+};
 
-// use rexpect::{error::Error, session::spawn_command};
+use rexpect::{error::Error, session::spawn_command};
+
+#[cfg(debug_assertions)]
+const PROFILE: &str = "debug";
+
+#[cfg(not(debug_assertions))]
+const PROFILE: &str = "release";
+
+fn exe<I>(cwd: impl AsRef<Path>, args: I) -> Command
+where
+    I: IntoIterator<Item = &'static str>,
+{
+    let base_dir = env!("CARGO_MANIFEST_DIR");
+    let bin = format!("{base_dir}/target/{PROFILE}/ci-tmp-test");
+    println!("{bin}");
+
+    let mut cmd = Command::new(bin);
+    cmd.current_dir(cwd).args(args);
+    cmd
+}
 
 #[test]
-fn exo_smoke_tests() {
+fn exo_smoke_tests() -> Result<(), Error> {
     let cargo_tmp_dir = env!("CARGO_TARGET_TMPDIR");
     let tmp_dir = tempfile::tempdir_in(cargo_tmp_dir).expect("Failed to create tempdir");
 
     assert!(tmp_dir.path().exists(), "Tmp dir not found");
     assert!(tmp_dir.path().is_dir(), "Tmp dir not a directory");
 
+    let cmd = exe(tmp_dir.path(), ["mariposas"]);
+    let mut p = spawn_command(cmd, Some(5000))?;
+    let output = p.exp_eof()?;
+    dbg!(output);
+
     let mut project_dir = PathBuf::from(tmp_dir.path());
     project_dir.push("mariposas");
-    std::fs::create_dir(project_dir.clone()).expect("Failed to create project_dir");
-    assert!(project_dir.exists(), "Project dir {project_dir:?} not found");
-    assert!(project_dir.is_dir(), "Project dir {project_dir:?} not a directory");
+    // std::fs::create_dir(project_dir.clone()).expect("Failed to create project_dir");
+    assert!(
+        project_dir.exists(),
+        "Project dir {project_dir:?} not found"
+    );
+    assert!(
+        project_dir.is_dir(),
+        "Project dir {project_dir:?} not a directory"
+    );
+    Ok(())
 }
